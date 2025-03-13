30c4519a10b911ac1a9bdf5064b08845a4c3fd9b
AlexanderArrr <alexanderaretzger@gmail.com>
2025-03-13 18:42:33 +0100

    User registration command

--- handler_user.go
+++ handler_user.go
@@ -1,6 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/AlexanderArrr/blog_aggregator_cli/internal/database"
+	"github.com/google/uuid"
+)
 
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) != 1 {
@@ -9,11 +17,44 @@ func handlerLogin(s *state, cmd command) error {
 
 	userName := cmd.args[0]
 
-	err := s.cfg.SetUser(userName)
+	_, err := s.db.GetUser(context.Background(), userName)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	err = s.cfg.SetUser(userName)
 	if err != nil {
 		return err
 	}
 
 	fmt.Printf("The Username %v has been set!\n", userName)
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("handlerRegister expected 1 argument, received %v", len(cmd.args))
+	}
+
+	userParams := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      cmd.args[0],
+	}
+	_, err := s.db.GetUser(context.Background(), userParams.Name)
+	if err == nil {
+		os.Exit(1)
+	}
+	_, err = s.db.CreateUser(context.Background(), userParams)
+	if err != nil {
+		return err
+	}
+
+	s.cfg.SetUser(cmd.args[0])
+	fmt.Printf("A user with the name %v was created!\n", cmd.args[0])
+	fmt.Printf("User ID: %v\nCreated at: %v\nUpdated at: %v\nName: %v\n",
+		userParams.ID, userParams.CreatedAt, userParams.UpdatedAt, userParams.Name)
+
 	return nil
 }
--- main.go
+++ main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/AlexanderArrr/blog_aggregator_cli/internal/config"
+	"github.com/AlexanderArrr/blog_aggregator_cli/internal/database"
+	_ "github.com/lib/pq"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
 func main() {
+	// Config and State initialization
 	cfg, err := config.Read()
 	if err != nil {
 		fmt.Println(err)
@@ -22,10 +27,20 @@ func main() {
 		cfg: &cfg,
 	}
 
+	// Registering commands
 	cmds := commands{
 		registeredCommands: make(map[string]func(*state, command) error),
 	}
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
+
+	// Database connection
+	db, err := sql.Open("postgres", cfg.DbUrl)
+	if err != nil {
+		fmt.Println("Error while loading database")
+	}
+
+	programState.db = database.New(db)
 
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: gator <command> [args...]")
